ca2b62b63f88cd6211f999426ed6eadccfa30aae
wuqf <wuqingfeng234@163.com>
2022-06-27 14:44:38 +0800

    modify the label of the normal distribution

--- normal-distribution.py
+++ normal-distribution.py
@@ -11,17 +11,16 @@ from scipy import stats
 import matplotlib.pyplot as plt
 import math
 
-
-# normal distribution 
+# normal distribution
 # z distribution if u is 0 and sig is 1
 u = 0
-sig = math.sqrt(1)
+sig = math.sqrt(4)
 
-x = np.linspace(u-5*sig, u+5*sig, 2000)
+x = np.linspace(u - 5 * sig, u + 5 * sig, 2000)
 y1 = stats.norm.pdf(x, u, sig)
 
 plt.rc('font', family='Arial Unicode MS', size=14)
-plt.plot(x, y1, label='N(0,1)')
+plt.plot(x, y1, label='N({},{})'.format(u, sig))
 plt.title("z-distribution")
 plt.legend()
 plt.grid(True)
