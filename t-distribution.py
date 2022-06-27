2e756796dbedbfdca0f5191f48e5f64704c8653a
wuqf <wuqingfeng234@163.com>
2022-06-27 14:48:26 +0800

    format files

--- t-distribution.py
+++ t-distribution.py
@@ -5,10 +5,10 @@ from scipy.stats import t
 import matplotlib.pyplot as plt
 
 print('比较t-分布与标准正态分布')
-x = np.linspace( -3, 3, 100)
-plt.plot(x, t.pdf(x,1), label='df=1')
-plt.plot(x, t.pdf(x,2), label='df=20')
-plt.plot(x, t.pdf(x,100), label = 'df=100')
-plt.plot( x[::5], norm.pdf(x[::5]),'kx', label='normal')
+x = np.linspace(-3, 3, 100)
+plt.plot(x, t.pdf(x, 1), label='df=1')
+plt.plot(x, t.pdf(x, 2), label='df=20')
+plt.plot(x, t.pdf(x, 100), label='df=100')
+plt.plot(x[::5], norm.pdf(x[::5]), 'kx', label='normal')
 plt.legend()
 plt.show()
